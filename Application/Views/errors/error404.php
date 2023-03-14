0611a0c362d7d2689a22fdb32930debf1139663a
Mahdiware <mahdiware@gmail.com>
2023-03-14 22:46:18 +0300

    404 not found page arranged

--- Application/Views/errors/error404.php
+++ Application/Views/errors/error404.php
@@ -1,10 +1,12 @@
 <?= $this->extend('layouts/layout') ?>
 
 <?= $this->section('content') ?>
+
 <div class="container">
     <div class="span">
         <h1>Error 404</h1>
         <p>The page you are looking for isn&#039;t found here!</p>
     </div>
 </div>
+
 <?= $this->endSection() ?>
