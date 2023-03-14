f0bde744b7652eaa865fb64e1769dd0746e26dae
Mahdiware <mahdiware@gmail.com>
2023-03-14 14:12:02 +0300

    updated autoload

--- Application/src/system/autoload.php
+++ Application/src/system/autoload.php
@@ -7,33 +7,22 @@ spl_autoload_register(function ($className){
     if(file_exists($library)){
     	require_once $library;
     }
+    
     # -------------------------------------------
     # LOAD Mahdiware
     # -------------------------------------------
-    $System_autoload = __DIR__.str_replace('\\', '/', (str_replace('Mahdiware\\', '/', $className))).".php";
+    $System_autoload = System . str_replace('\\', '/', (str_replace('Mahdiware\\', '/', $className))).".php";
     if (file_exists($System_autoload)){
         require_once $System_autoload;
     }
-    // echo  $System_autoload;
     
     # -------------------------------------------
     # LOAD Application
     # -------------------------------------------
-    $Application_autoload = PATH . str_replace('\\', '/', ($className)).".php";
-    
-    if(file_exists($Application_autoload)){
-        require_once $Application_autoload;
-    }
-    
-    # -------------------------------------------
-    # LOAD Views
-    # -------------------------------------------
-    $Views_autoload = $Os->ViewsPath . str_replace('\\', '/', ($className)).".php";
-    
+    $Views_autoload = str_replace('\\', '/', ($className)).".php";
     if(file_exists($Views_autoload)){
         require_once $Views_autoload;
     }
-    // echo  $Views_autoload;
 
 });
 ?>
