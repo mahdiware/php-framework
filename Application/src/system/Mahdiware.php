62450ee49c6e7f4483b339db5f3ca5715bb82191
Mahdiware <mahdiware@gmail.com>
2023-03-14 11:14:02 +0300

    framework head

--- Application/src/system/Mahdiware.php
+++ Application/src/system/Mahdiware.php
@@ -6,7 +6,7 @@ use Application\Controllers\Routes as Router;
 use Mahdiware\Routes;
 
 class Mahdiware {
-	public const VERSION = '4.0';
+	public const VERSION = '4.1';
     private $ErrorPage; //ErrorPage
     private $Routes;
     private $data;
