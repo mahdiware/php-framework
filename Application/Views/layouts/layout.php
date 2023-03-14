c14b94c649768475b8753bc2d19a4d3150882d00
Mahdiware <mahdiware@gmail.com>
2023-03-14 22:48:35 +0300

    extend page arranged

--- Application/Views/layouts/layout.php
+++ Application/Views/layouts/layout.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?= $title ?></title>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?= $title ?></title>
     
-	<?= $this->renderSection('css') ?>
+		<?= $this->renderSection('css') ?>
 	
-	<?= $this->renderSection('js') ?>
-</head>
-<body class="grey lighten-3">
+		<?= $this->renderSection('js') ?>
+	</head>
+	<body class="grey lighten-3 ">
 	
-	<?= $this->renderSection('content') ?>
+		<?= $this->renderSection('content') ?>
 	
-</body>
+	</body>
 </html>
