58887e36808ea3dc4f4efdbf29e5335d3612f57e
Mahdiware <mahdiware@gmail.com>
2023-03-14 22:45:20 +0300

    access Denied page -> i was arranged

--- Application/Views/errors/accessDenied.php
+++ Application/Views/errors/accessDenied.php
@@ -1,10 +1,12 @@
 <?= $this->extend('layouts/layout') ?>
 
 <?= $this->section('content') ?>
+
 <div class="container">
     <div class="span">
         <h1>Access Denied</h1>
         <p>Oops, you can&#039;t be here!</p>
     </div>
 </div>
+
 <?= $this->endSection() ?>
