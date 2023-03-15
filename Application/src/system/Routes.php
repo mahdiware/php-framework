fc30708686ec5b3c1919ee75f898025a72a83f67
Mahdiware <mahdiware@gmail.com>
2023-03-15 21:15:14 +0300

    Routes class updated

--- Application/src/system/Routes.php
+++ Application/src/system/Routes.php
@@ -7,7 +7,6 @@ class Routes
     private $Routes= array();
     
     public function set($type, $name, $direction){
-        #$routes->match(['get', 'post'], 'lib', 'FileUpload::index');
         $data = [
             'type' => strtoupper($type),
             'name' => $name,
