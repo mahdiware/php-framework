935ee59c138ea75a605b1a49717d13fe4bed2ed5
Mahdiware <mahdiware@gmail.com>
2023-03-31 15:59:58 +0300

    testing Session();

--- Application/Controllers/Home.php
+++ Application/Controllers/Home.php
@@ -2,14 +2,26 @@
 namespace Application\Controllers;
 use Mahdiware\Controller;
 use Mahdiware\Routes;
+use Mahdiware\Session;
 use Application\Libraries\TestLib;
 
 class Home extends Controller {
 
     function Index() { 
-    
+    	
+    	// Create a new session instance
+    	$Session = new Session();
+
+    	// Check if the session variable "visited" is null
+    	if($Session->get("visited") == null){
+    		$Session->set("visited", 1); // If so, set it to 1
+    	}else{
+    		$Session->set("visited", 1+($Session->get("visited"))); // If not, increment the value of "visited"
+    	}
+
         $content = [
         	"title" => "Welcome To Mahdiware",
+        	"visited" => $Session->get("visited"),
         ];
         $this->view("index", $content);
         
